476a720f5e8dc0a623a05c74615c90d4cc362f31
Franz Rodrigo <franzrodrigg@gmail.com>
2026-09-09 22:08:41 -0400

    Definicion Clase

---
+++ torre.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Torre
+	
+{
+	
+public:
+	
+	short fila;
+	
+	short columna;
+	
+private:
+	
+	bool isBlack;
+	
+};
+
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
